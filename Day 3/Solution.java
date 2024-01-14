199c5db67a6baaded6240f99ae3f8b94b7da1070
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-01-14 16:55:11 +0530

    Create Solution.java

---
+++ Day 3/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (char ch : word1.toCharArray()) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2.toCharArray()) {
+            freq2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
