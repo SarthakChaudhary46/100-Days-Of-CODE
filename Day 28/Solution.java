965646d95df294b049565f379c2949b20f661a87
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-02-08 10:36:03 +0530

    Solution!!

---
+++ Day 28/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            int min_val = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+                min_val = Math.min(min_val, dp[i - j * j] + 1);
+            }
+            dp[i] = min_val;
+        }
+        return dp[n];
+    }
+}
