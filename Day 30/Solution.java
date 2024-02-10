9ac7ffee569b05f38b86fb851879e23615529489
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-02-10 08:59:58 +0530

    Solution!!

---
+++ Day 30/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+
+        // Every single character is a palindrome
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            count++;
+        }
+
+        // Check for palindromes of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                count++;
+            }
+        }
+
+        // Check for palindromes of length greater than 2
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
