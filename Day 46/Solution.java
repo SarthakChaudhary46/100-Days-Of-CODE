e4e8e2e64e93afbe27a0188073a9edd5b0d5dd5d
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-02-26 10:21:32 +0530

    Create Solution.java

---
+++ Day 46/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
