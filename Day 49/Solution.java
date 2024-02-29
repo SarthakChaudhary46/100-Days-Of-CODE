976430c14b23f150b377385f9b10f7591534493b
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-02-29 09:58:18 +0530

    Create Solution.java

---
+++ Day 49/Solution.java
@@ -0,0 +1,70 @@
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean flag = true;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int prev = flag ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (flag) {
+                    if (node.val % 2 == 0 || node.val <= prev) {
+                        return false;
+                    }
+                } else {
+                    if (node.val % 2 != 0 || node.val >= prev) {
+                        return false;
+                    }
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                prev = node.val;
+            }
+            flag = !flag;
+        }
+        return true;
+    }
+}
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean flag = true;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int prev = flag ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (flag) {
+                    if (node.val % 2 == 0 || node.val <= prev) {
+                        return false;
+                    }
+                } else {
+                    if (node.val % 2 != 0 || node.val >= prev) {
+                        return false;
+                    }
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                prev = node.val;
+            }
+            flag = !flag;
+        }
+        return true;
+    }
+}
