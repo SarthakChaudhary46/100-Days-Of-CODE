ce2dca619bba1fed1edf376d8b6b3c1193f52ab4
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-03-20 10:27:09 +0530

    Create Solution.java

---
+++ Day 69/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode currNode = list1;
+        int index = 0;
+        while (index < a - 1) {
+            currNode = currNode.next;
+            index++;
+        }
+        ListNode front = currNode;
+        while (index < b + 1) {
+            currNode = currNode.next;
+            index++;
+        }
+        ListNode rear = currNode;
+        ListNode secondListTail = list2, secondListHead = list2;
+        while (secondListTail.next != null) {
+            secondListTail = secondListTail.next;
+        }
+        front.next = secondListHead;
+        secondListTail.next = rear;
+        return list1;
+    }
+}
