2104d45dbe55b5fcca0571f56765bb7e9df8bf18
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-03-21 10:26:51 +0530

    Create Solution.java

---
+++ Day 70/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+        while (curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
