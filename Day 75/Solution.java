0d1a455c9ec4d6b9b9ba3c2bdd68b81b566f2b23
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-03-26 11:01:24 +0530

    Create Solution.java

---
+++ Day 75/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    // Function to swap elements in the array
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        // Place each positive integer i at index i-1 if possible
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        // Find the first missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // If all positive integers from 1 to n are present, return n + 1
+        return n + 1;
+    }
+}
+
