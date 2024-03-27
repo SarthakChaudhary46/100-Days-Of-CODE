79d13f27e29404bb1afb6e58adc2bd6789daf656
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-03-27 15:44:31 +0530

    Create Solution.java

---
+++ Day 76/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+
+        int left = 0, right = 0, product = 1, count = 0;
+        int n = nums.length;
+
+        while (right < n) {
+            product *= nums[right];
+            while (product >= k) product /= nums[left++];
+            count += 1 + (right - left);
+            right++;
+        }
+
+        return count;
+    }
+}
