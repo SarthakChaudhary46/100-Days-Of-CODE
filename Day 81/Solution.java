39125ace86048cc8e1978cd9b3b9e21bd35a76d1
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-04-01 10:14:13 +0530

    Create Solution.java

---
+++ Day 81/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+        
+        return length;
+    }
+}
