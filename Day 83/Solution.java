f5e35572609e56d0c3e624854023c320de935478
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-04-03 10:18:08 +0530

    Create Solution.java

---
+++ Day 83/Solution.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        
+        visited[i][j] = true;
+        
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+            backtrack(board, word, visited, i - 1, j, index + 1) ||
+            backtrack(board, word, visited, i, j + 1, index + 1) ||
+            backtrack(board, word, visited, i, j - 1, index + 1)) {
+            return true;
+        }
+        
+        visited[i][j] = false;
+        return false;
+    }
+}
