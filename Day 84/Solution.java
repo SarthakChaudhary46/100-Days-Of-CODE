500638a928fa5cb3336bed54555ab8b6d50be938
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-04-04 10:29:41 +0530

    Create Solution.java

---
+++ Day 84/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count = 0;
+        int maxNum = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+                if (maxNum < count)
+                    maxNum = count;
+            } else if (c == ')') {
+                count--;
+            }
+        }
+        return maxNum;
+    }
+}
