2ac52387fe4f47d96538a04617754104de9f804e
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-04-09 10:01:55 +0530

    Create Solution.java

---
+++ Day 89/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int total = 0;
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                total += Math.min(tickets[i], tickets[k]);
+            } else {
+                total += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return total;
+    }
+}
